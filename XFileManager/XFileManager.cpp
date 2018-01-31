5fabc59fced1375fff72bf44fab5ffa6413d4eb0
illidan <smile@illidan.org>
2018-01-31 12:54:42 +0800

    2.2-添加注释。

--- XFileManager/XFileManager.cpp
+++ XFileManager/XFileManager.cpp
@@ -21,24 +21,27 @@ int XFileManager::XDone()
 
 int XFileManager::XLoadImage(XOUT unsigned char*& pBuffer, XOUT int& w, XOUT int& h, XIN const char* pFile)
 {
+	//判断文件类型
 	FREE_IMAGE_FORMAT fif = FIF_UNKNOWN;
-
 	fif = FreeImage_GetFileType(pFile);
 	if (fif == FIF_UNKNOWN)
 		fif = FreeImage_GetFIFFromFilename(pFile);
 	if (fif == FIF_UNKNOWN)
 		return -1;
 
+	//加载文件
 	FIBITMAP* dib = NULL;
 	if (FreeImage_FIFSupportsReading(fif))
 		dib = FreeImage_Load(fif, pFile);
 	if (!dib)
 		return -2;
 
+	//转换文件格式
 	FIBITMAP* temp = dib;
 	dib = FreeImage_ConvertTo32Bits(dib);
 	FreeImage_Unload(temp);
 
+	//获取文件信息
 	BYTE* bit = FreeImage_GetBits(dib);
 	w = FreeImage_GetWidth(dib);
 	h = FreeImage_GetHeight(dib);
@@ -46,10 +49,12 @@ int XFileManager::XLoadImage(XOUT unsigned char*& pBuffer, XOUT int& w, XOUT int
 	if ((bit == 0) || (w == 0) || (h == 0))
 		return -3;
 
+	//保存文件
 	size_t size = w * h * 4;
 	pBuffer = new unsigned char[size];
 	memcpy(pBuffer, bit, size);
 
+	//释放资源
 	FreeImage_Unload(dib);
 
 	return 0;
@@ -57,8 +62,13 @@ int XFileManager::XLoadImage(XOUT unsigned char*& pBuffer, XOUT int& w, XOUT int
 
 int XFileManager::XSaveImage(XOUT unsigned char*& pBuffer, XOUT int& w, XOUT int& h, XIN const char* pFile)
 {
+	//生成一张深度32位的图片
 	FIBITMAP* dib = FreeImage_Allocate(w, h, 32);
+
+	//获取文件指针
 	unsigned char* pPixel = FreeImage_GetBits(dib);
+
+	//保存内容
 	size_t size = w * h;
 	for (unsigned int i = 0; i < size; ++i)
 	{
@@ -67,7 +77,11 @@ int XFileManager::XSaveImage(XOUT unsigned char*& pBuffer, XOUT int& w, XOUT int
 		pPixel[i * 4 + 2] = pBuffer[i * 4 + 2];
 		pPixel[i * 4 + 3] = pBuffer[i * 4 + 3];
 	}
+
+	//生成文件
 	FreeImage_Save(FIF_PNG, dib, pFile, PNG_DEFAULT);
+
+	//释放资源
 	FreeImage_Unload(dib);
 
 	return 0;
